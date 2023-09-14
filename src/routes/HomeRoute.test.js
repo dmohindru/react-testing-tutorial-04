c9cf99d6c524e025ecff05eeaeadb677d5bdefcc
Dhruv Mohindru <dmohindru@gmail.com>
2023-09-15 06:33:18 +1000

    Making progress

--- src/routes/HomeRoute.test.js
+++ src/routes/HomeRoute.test.js
@@ -1,38 +1,22 @@
 import { render, screen } from '@testing-library/react';
-import { setupServer } from 'msw/node';
-import { rest } from 'msw';
 import { MemoryRouter } from 'react-router-dom';
 import HomeRoute from './HomeRoute';
+import { createServer } from '../test/server';
 
-const handlers = [
-    rest.get('/api/repositories', (req, res, ctx) => {
-    
+createServer([
+    {
+       path: '/api/repositories',
+       res: (req) => {
         const language = req.url.searchParams.get('q').split('language:')[1];
-
-        return res(
-            ctx.json({
-                items: [
-                    {id: 1, full_name: `${language}_one`},
-                    {id: 2, full_name: `${language}_two`}
-                ]
-            })
-        );
-    }),
-];
-
-const server = setupServer(...handlers);
-
-beforeAll(() => {
-    server.listen();
-});
-
-afterEach(() => {
-    server.resetHandlers();
-});
-
-afterAll(() => {
-    server.close();
-});
+        return {
+            items: [
+                {id: 1, full_name: `${language}_one`},
+                {id: 2, full_name: `${language}_two`}
+            ],
+        };
+       }, 
+    },
+]);
 
 test('renders two links for each language', async () => {
     render(
@@ -58,7 +42,6 @@ test('renders two links for each language', async () => {
             name: new RegExp(`${language}_`)
         });
 
-        
         expect(links).toHaveLength(2);
         expect(links[0]).toHaveTextContent(`${language}_one`);
         expect(links[1]).toHaveTextContent(`${language}_two`);
@@ -71,7 +54,3 @@ test('renders two links for each language', async () => {
 
 
 });
-
-const pause = () => new Promise(resolve => {
-    setTimeout(resolve, 100);
-})
